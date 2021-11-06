163c52bf288b0ce5c4d4ba286b17a5743ebd663d
Adrian Lin <adrianylin@gmail.com>
2021-11-06 13:02:32 -0700

    particles finishline and player crash

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+    [SerializeField] float reloadSceneDelay = 2f;
+    [SerializeField] ParticleSystem finishEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            finishEffect.Play();
+            Invoke("ReloadScene", reloadSceneDelay);
+
+            Debug.Log("Finished");
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
